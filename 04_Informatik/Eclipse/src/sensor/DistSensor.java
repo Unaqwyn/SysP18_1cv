521bcb1ebd8b1c4c33f19de1b111f06febe5831b
Romario B. Jäger <jaeger.romario@ntb.ch>
2018-03-06 15:55:50 +0100

    touch programm

--- 04_Informatik/Eclipse/src/sensor/DistSensor.java
+++ 04_Informatik/Eclipse/src/sensor/DistSensor.java
@@ -6,7 +6,8 @@ import ch.ntb.inf.deep.runtime.mpc555.driver.HLC1395Pulsed;
 import ch.ntb.inf.deep.runtime.mpc555.driver.SCI;
 import ch.ntb.inf.deep.runtime.ppc32.Task;
 
-public class DistSensor {
+public class DistSensor extends Task
+{
 
 	// I/Os to use:
 	private final int ADR3PIN = -1; // not used
@@ -16,14 +17,56 @@ public class DistSensor {
 	private final int TRGPIN = 5; // MPIOB5
 	private final int ANPIN = 59; // A:AN59
 	private HLC1395Pulsed hlc1395;
-	private int grenzwert=100;
+	int[] werte;
+	double durchschnitt = 0;
+	public static boolean angestossen = false;
 
-	public boolean isTouched() {
-		return (hlc1395.read(0)>=grenzwert);
+	public void action()
+	{
+		// System.out.println(hlc1395.read(3));
+		for (int i = 0; i < 10; i++)
+		{
+			if (!contains(0))
+			{
+				durchschnitt -= werte[i] / 10;
+			}
+			werte[i] = hlc1395.read(3);
+			durchschnitt += werte[i] / 10;
+			if (werte[i] >= durchschnitt * 1.2)
+			{
+				// System.out.print("hinderniss erkannt: ");
+				// System.out.print(werte[i]);
+				// System.out.print(durchschnitt);
+				// System.out.println();
+				angestossen = true;
+			}
+		}
+
 	}
 
+	public boolean hindernis()
+	{
+		if (angestossen)
+		{
+			angestossen = false;
+			return true;
+		}
+		return false;
+	}
 
-	public DistSensor() {
+	public boolean contains(int k)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			if (werte[i] == k)
+				return true;
+
+		}
+		return false;
+	}
+
+	public DistSensor()
+	{
 		hlc1395 = HLC1395Pulsed.getInstance();
 
 		// Initialize sensors
@@ -31,15 +74,21 @@ public class DistSensor {
 
 		// Start reading
 		hlc1395.start();
+		werte = new int[10];
 	}
 
-	static {
+	static
+	{
 		// 1) Initialize SCI1 (9600 8N1)
 		SCI sci1 = SCI.getInstance(SCI.pSCI1);
 		sci1.start(9600, SCI.NO_PARITY, (short) 8);
 
 		// 2) Use SCI1 for stdout
 		System.out = new PrintStream(sci1.out);
+
+		Task t = new DistSensor();
+		t.period = 100;
+		Task.install(t);
 
 		// Initialize task
 	}
--- 04_Informatik/Eclipse/src/test/TestTouch.java
+++ 04_Informatik/Eclipse/src/test/TestTouch.java
@@ -5,25 +5,30 @@ import java.io.PrintStream;
 import ch.ntb.inf.deep.runtime.mpc555.driver.MPIOSM_DIO;
 import ch.ntb.inf.deep.runtime.mpc555.driver.SCI;
 import ch.ntb.inf.deep.runtime.ppc32.Task;
+import sensor.DistSensor;
 
-public class TestTouch extends Task{
-	final static short pinA=5, pinB=6;
+public class TestTouch extends Task
+{
+	final static short pinA = 9, pinB = 6;
 	public static MPIOSM_DIO ledA, touchB;
-	
+	private DistSensor distSensor;
+
 	public TestTouch()
 	{
-		ledA=new MPIOSM_DIO(pinA,true);
+		ledA = new MPIOSM_DIO(pinA, true);
 		ledA.set(false);
-		touchB=new MPIOSM_DIO(pinB, false);
-	}
-	
-	public void action()
-	{
-		ledA.set(!touchB.get());
-		System.out.println(touchB.get());
+		touchB = new MPIOSM_DIO(pinB, false);
+		distSensor = new DistSensor();
 	}
 
-	static {
+	public void action()
+	{
+		ledA.set(distSensor.hindernis());
+		//distSensor.angestossen=false;
+	}
+
+	static
+	{
 
 		// SCI1 initialisieren und als Standardausgabe verwenden
 		// 1) Initialize SCI1 (9600 8N1)
@@ -34,7 +39,7 @@ public class TestTouch extends Task{
 		System.out = new PrintStream(sci1.out);
 
 		Task task = new TestTouch();
-		task.period=400;
+		task.period = 100;
 		Task.install(task);
 	}
 }
