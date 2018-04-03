b9c1e37a0c9a6645b81cf3192e0c52b30a05583c
Romario B. Jäger <jaeger.romario@ntb.ch>
2018-04-03 10:55:22 +0200

    sensoren updated

--- 04_Informatik/RobiNew/src/Sensor/DistSensor.java
+++ 04_Informatik/RobiNew/src/Sensor/DistSensor.java
@@ -5,24 +5,50 @@ import java.io.PrintStream;
 import Definitions.PinMap;
 import ch.ntb.inf.deep.runtime.mpc555.driver.HLC1395Pulsed;
 import ch.ntb.inf.deep.runtime.mpc555.driver.SCI;
+import ch.ntb.inf.deep.runtime.ppc32.Task;
 
-public class DistSensor
+public class DistSensor extends Task
 {
 	private int sensorNR;
-	private int[] werte;
-	
+	private int[] value;
+	private double average = 0;
+	private boolean obstacle = false;
+	private double k=1.2;
+
 	public DistSensor(int i)
 	{
-		sensorNR=i;
+		sensorNR = i;
+		Task t = new DistSensor(i);
+		t.period = 100;
+		Task.install(t);
 	}
 
 	public boolean obstacle()
 	{
-		return true;
+		if(obstacle)
+		{
+			obstacle=false;
+			return true;
+		}
+		return false;
 	}
-	
+
 	public int readSensor()
 	{
-		return 0;
+		return Sensoren.sensorValues[sensorNR];
+	}
+
+	public void action()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			if (readSensor() >= average*k)
+			{
+				obstacle = true;
+			}
+			average -= (value[i] * 0.1);
+			value[i] = readSensor();
+			average += (value[i] * 0.1);
+		}
 	}
 }
